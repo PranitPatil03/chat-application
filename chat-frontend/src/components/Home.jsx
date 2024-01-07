4c062e33d5b629655074ba4611c54625e501abb2
Pranit <patilpranit3112@gmail.com>
2024-01-07 15:56:35 +0530

    Home Page - Sign-up Page UI Started

--- chat-frontend/src/components/Home.jsx
+++ chat-frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+/* eslint-disable react/jsx-no-comment-textnodes */
 import Navbar from "./Navbar";
 import { user1 } from "../assets/index.js";
 import { user2 } from "../assets/index.js";
@@ -10,7 +10,7 @@ const Home = () => {
     <>
       <Navbar />
 
-      <div className="flex items-center justify-between gap-10 py-8 px-5">
+      <div className="flex items-center gap-[15rem] py-8 px-5">
         <div className="flex flex-col items-center justify-center gap-5 my-[12%] mx-[4%] leading-7">
           <div className="text-black text-6xl">
             <h2 className="leading-[4rem]">
@@ -40,7 +40,37 @@ const Home = () => {
           </div>
         </div>
 
-        <div className=""></div>
+        <div className="flex px-6 border-grey w-[500px] h-[400px] shadow-md">
+          <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold">Join Us!</h1>
+            <p className="mb-4">
+              We're looking for amazing engineers just like you!
+            </p>
+
+            <div className="bg-white rounded-lg p-8">
+              <form className="flex">
+                <div className="flex flex-col w-full">
+                  <div className="flex flex-col w-full">
+                    <p className="text-black">First Name</p>
+                    <input
+                      type="text"
+                      name="firstName"
+                      className="w-full outline-none border-[1px] border-grey rounded-md"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-black">First Name</p>
+                    <input
+                      type="text"
+                      name="firstName"
+                      className="w-full outline-none border-[1px] border-grey rounded-md"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
